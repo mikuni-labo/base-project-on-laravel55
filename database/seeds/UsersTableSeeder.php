5bc6720a97cc3bdcff310150cd32f09d8024e5f3
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-18 16:09:52 +0900

    シード作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('users')->truncate();
+        Schema::enableForeignKeyConstraints();
+
+        DB::table('users')->insert([
+            [
+                'id'           => 1,
+                'name'         => '和田邦康',
+                'email'        => 'k-wada@mikunilabo.com',
+                'password'     => bcrypt('p1p1p1p1'),
+                'created_at'   => Carbon::now(),
+                'updated_at'   => Carbon::now(),
+            ],
+        ]);
+    }
+}
