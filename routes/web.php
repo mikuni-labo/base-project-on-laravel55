187df8f11d83ad83381a103320403a574ce0e068
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-18 13:20:19 +0900

    組み込み認証機能を導入

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::view('/',   'welcome');
+Route::get('home', 'HomeController@index')->name('home');
+
+Auth::routes();
