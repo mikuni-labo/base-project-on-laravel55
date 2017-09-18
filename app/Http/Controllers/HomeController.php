0ff874cc00eaaea31e42d7ae6a6d0e520fc38724
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-18 22:11:31 +0900

    mailableのレンダリング機能をテスト

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,16 +28,4 @@ class HomeController extends Controller
         return view('home');
     }
 
-    /**
-     * Show phpinfo.
-     *
-     * @method GET
-     * @param Request $request
-     * @return void
-     */
-    public function phpinfo(Request $request) : void
-    {
-        phpinfo();
-    }
-
 }
