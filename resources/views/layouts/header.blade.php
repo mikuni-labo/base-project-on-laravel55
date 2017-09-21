cc1f6de8506824f85c737116e6571d14465faa5f
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-21 12:50:52 +0900

    ヘッダー整備

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
 
@@ -11,33 +11,60 @@
             </button>
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+            <a class="navbar-brand" href="{{ route('home') }}">
+                {{ config('app.name') }}
+
+                @if( App::isDownForMaintenance() )
+                    <small>
+                        （<span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>メンテナンスモード）
+                    </small>
+                @endif
+            </a>
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                &nbsp;
+                @if( Auth::check() )
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&nbsp;メニュー&nbsp;<span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ route('passport') }}"><span class="glyphicon glyphicon-plane" aria-hidden="true"></span>&nbsp;パスポート管理</a></li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
+                <li class="dropdown">
+                    @auth
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            <span class="glyphicon glyphicon-th" aria-hidden="true"></span>&nbsp;{{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-
                         <ul class="dropdown-menu" role="menu">
+                            <li class="dropdown-header">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;{{ Auth::user()->name }}&nbsp;としてログインしています。
+                            </li>
+
+                            <li role="separator" class="divider"></li>
+
+                            <li><a href="{{ route('home') }}"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;ホーム</a></li>
+                            <li><a href="{{ route('phpinfo') }}"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>&nbsp;phpinfo</a></li>
+
+                            <li role="separator" class="divider"></li>
+
                             <li>
-                                <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    ログアウト
+                                @if(false)
+                                    <a href="{{ route('logout') }}" onclick="return false;" id="logoutBtn">
+                                        <span class="glyphicon glyphicon-off" aria-hidden="true"></span>&nbsp;ログアウト
+                                    </a>
+                                @endif
+
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <span class="glyphicon glyphicon-off" aria-hidden="true"></span>&nbsp;ログアウト
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -45,8 +72,16 @@
                                 </form>
                             </li>
                         </ul>
-                    </li>
-                @endguest
+                    @else
+                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="glyphicon glyphicon-th" aria-hidden="true"></span>&nbsp;GUEST <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;ログイン</a></li>
+                            <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;登録</a></li>
+                        </ul>
+                    @endguest
+                </li>
             </ul>
         </div>
     </div>
