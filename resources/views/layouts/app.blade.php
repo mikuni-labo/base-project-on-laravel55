ebd5a178b73b91e7a84e742189af6843bafcc0af
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-21 12:11:24 +0900

    ビューのベースレイアウトを変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,13 @@
+{{ header('X-Frame-Options: SAMEORIGIN') }}
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    @yield('meta')
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
@@ -75,6 +74,12 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+    <script type="text/javascript">
+          window.Laravel = window.Laravel || {};
+          window.Laravel.csrfToken = "{{csrf_token()}}";
+    </script>
+
+    @yield('script')
 </body>
 </html>
--- resources/views/passport.blade.php
+++ resources/views/passport.blade.php
@@ -1,29 +1,23 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-        <link rel="stylesheet" href="css/app.css"/>
-        <script type="text/javascript">
-          window.Laravel = window.Laravel || {};
-          window.Laravel.csrfToken = "{{csrf_token()}}";
-        </script>
-    </head>
-    <body>
-        <div id="app">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <passport-clients></passport-clients>
-                        <passport-authorized-clients></passport-authorized-clients>
-                        <passport-personal-access-tokens></passport-personal-access-tokens>
-                    </div>
-                </div>
+@section('meta')
+    <title>Passport設定｜{{ config('app.name') }}</title>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
             </div>
         </div>
-    <script src="js/app.js"></script>
-    </body>
-</html>
+    </div>
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        //
+    </script>
+@endsection
