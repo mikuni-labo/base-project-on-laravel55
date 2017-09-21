cf2ea6c679c0edf5a2d1a72ac45a157e19bbc573
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-21 11:50:52 +0900

    環境毎にアセットコンパイル時の挙動を変えた

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+//Versioning
+if ( mix.inProduction() ) {
+    mix.version();
+} else {
+	mix.sourceMaps();
+}
