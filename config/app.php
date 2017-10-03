a4b0801853a3aadda250be70f0c0fb41e0718244
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-10-03 12:23:05 +0900

    v5.5からパッケージ自動認識機能があるので、config登録を廃止

--- config/app.php
+++ config/app.php
@@ -167,11 +167,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Collective\Html\HtmlServiceProvider::class,
-//         Intervention\Image\ImageServiceProvider::class,
-//         Laravel\Tinker\TinkerServiceProvider::class,
-        Laracasts\Flash\FlashServiceProvider::class,
-        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -241,7 +236,7 @@ return [
         'Flash'     => Laracasts\Flash\Flash::class,
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
-//         'Image'     => Intervention\Image\Facades\Image::class,
+        'Image'     => Intervention\Image\Facades\Image::class,
 
     ],
 
