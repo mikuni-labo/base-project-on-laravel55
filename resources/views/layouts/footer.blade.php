382af3e885685a284a8def7c691ad6c26c012ba6
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-10-04 22:57:30 +0900

    現在時刻のCarbonインスタンスを返すヘルパがあったなんて・・

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,7 @@
 <footer class="footer text-center">
     <div class="container">
-        <span id="scroll-top">
-            <a href="#"><i class="fa fa-angle-double-up"></i></a>
-        </span>
-
         <small>
-            &copy;&nbsp;{{ Carbon::now()->format('Y') }}&nbsp;
+            &copy;&nbsp;{{ now()->format('Y') }}&nbsp;
             <a href="{{ route('home') }}">{{ config('app.name') }}</a>
             &nbsp;All&nbsp;Rights&nbsp;Reserved.
         	</small>
