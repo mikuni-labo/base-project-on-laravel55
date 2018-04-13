6348e58447710fa9f03fcc23f9c9931ee82198b2
mikuni-labo <k-wada@mikunilabo.com>
2018-04-13 11:37:06 +0900

    所属するリレーションが存在しない場合に（論理削除含む）、プロパティ参照エラーなどを防ぐため、インスタンスをセットしておくように定義しておく

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -67,7 +67,7 @@ class Comment extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withDefault();
     }
 
     /**
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -60,7 +60,7 @@ class Post extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withDefault();
     }
 
     /**
