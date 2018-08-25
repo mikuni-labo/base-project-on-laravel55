13955252715bfd6156d6c6a81f85694dff8ec0d5
mikunilabo <k-wada@mikunilabo.com>
2018-08-25 23:49:00 +0900

    tcpdf-fpdiテスト

--- app/Http/Controllers/Test/PdfController.php
+++ app/Http/Controllers/Test/PdfController.php
@@ -5,17 +5,28 @@ namespace App\Http\Controllers\Test;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use setasign\Fpdi\Fpdi;
-use TCPDF;
+use setasign\Fpdi\TcpdfFpdi;
+use TCPDF_FONTS;
 
 class PdfController extends Controller
 {
+    /** @var TcpdfFpdi */
+    private $pdf;
+
+    /** @var TCPDF_FONTS */
+    private $fonts;
+
     /**
+     * @param TcpdfFpdi $pdf
+     * @param TCPDF_FONTS $fonts
      * @return void
      */
-    public function __construct()
+    public function __construct(TcpdfFpdi $pdf, TCPDF_FONTS $fonts)
     {
         $this->middleware('auth');
+
+        $this->pdf = $pdf;
+        $this->fonts = $fonts;
     }
 
     /**
@@ -23,8 +34,7 @@ class PdfController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $this->fpdi();
-//         $this->tcpdf();
+        $this->process();
 
         return view('pdf.test');
     }
@@ -32,34 +42,35 @@ class PdfController extends Controller
     /**
      * @return void
      */
-    private function fpdi()
+    private function process()
     {
-        $pdf = new Fpdi();
-        $pdf->setSourceFile(storage_path('system/pdf/postcards/postcard.pdf'));
-        $pdf->AddPage();
-        $pdf->SetMargins(0,0,0);
-        $importPage = $pdf->importPage(1);
-        $pdf->useTemplate($importPage, 0, 0, null, null, true);
-        $pdf->SetFont("helvetica", "", 12);
-        $pdf->Text(50, 100, "Hello World!!");
-        $pdf->Output('I', 'test.pdf');
-    }
+        $this->pdf->SetMargins(0,0,0);
+        $this->pdf->SetAutoPageBreak(false);
+        $this->pdf->setPrintHeader(false);
+        $this->pdf->setPrintFooter(false);
+        $this->pdf->setSourceFile(storage_path('system/pdf/postcards/postcard.pdf'));
+        $tpl = $this->pdf->importPage(1);
+//         $hanamina = $this->fonts->addTTFfont(storage_path('system/fonts/HanaMinA.ttf'));
 
-    /**
-     * @return void
-     */
-    private function tcpdf()
-    {
-        $pdf = new TCPDF();
-        $pdf->AddPage();
-        $pdf->SetMargins(0,0,0);
-        $pdf->setPrintHeader(false);
-        $pdf->setPrintFooter(false);
-        $pdf->SetFont("kozminproregular", "", 12);
-        $pdf->Text(50, 100, "テスト");
-//         $html = view('pdf.test');
-//         $pdf->writeHTML($html);
-        $pdf->Output("test.pdf");
+        // Start of loop.
+        $this->pdf->AddPage();
+        $this->pdf->useTemplate($tpl, 0, 0, null, null, true);
+
+//         $this->pdf->SetFont($hanamina, '', 24, '', true);
+        $this->pdf->SetFont("kozgopromedium", '', 24, '', true);
+        $this->pdf->setFontSpacing(3.5);
+        $this->pdf->text(44, 50, "test");
+
+//         $this->pdf->SetFont($hanamina, '', 20, '', true);
+        $this->pdf->SetFont("kozgopromedium", '', 20, '', true);
+        $this->pdf->setFontSpacing(3.5);
+        $this->pdf->text(45.0, 12.0, "5430001");
+
+        // End of loop.
+
+        if ($this->pdf->getPage()) {
+            $this->pdf->Output('test.pdf');
+        }
     }
 
 }
